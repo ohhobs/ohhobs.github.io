d9e8339bf3a1b5bc82faa98c5d17d9b57e6f736c
Hyun Ho Oh <hyunho.oh@renewtrak.com>
2024-04-08 21:51:45 +1000

    feature/multi-note

--- source/src/app/note/page.tsx
+++ source/src/app/note/page.tsx
@@ -1,41 +1,115 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect, ChangeEvent } from 'react'
 import Link from 'next/link'
-import { save, load } from '@/app/note/services/NoteService'
+import type Note from '@/app/note/models/Note'
+import { loadAllNote, saveNote, updateNote, deleteNote } from '@/app/note/services/NoteService'
 
 export default function Note() {
 
-  const [note, setNote] = useState<string>('')
+  const [notes, setNotes] = useState<Note[]>([])
+  const [newNoteName, setNewNoteName] = useState<string>('')
+  const [selectedNote, setSelectedNote] = useState<Note | null>(null)
+  const [selectedNoteContent, setSelectedNoteContent] = useState<string>('')
 
   useEffect(() => {
-    const content = load()
-    setNote(content)
+    setAllNotes()
   }, [])
+
+  function setAllNotes(): void {
+    const notes: Note[] = loadAllNote()
+    setNotes(notes)
+  }
+
+  function _updateNote(note: Note): void {
+    updateNote(note)
+    setAllNotes()
+  }
+
+  function handleCreateNote(name: string, content: string): void {
+    if (name === '') {
+      alert('Empty name')
+    } else {
+      const note: Note = { id: '', name, content }
+      saveNote(note)
+      setAllNotes()
+    }
+  }
+
+  function selectNote(note: Note): void {
+    setSelectedNote(note)
+    setSelectedNoteContent(note.content)
+  }
+
+  function handleContentChange(e: ChangeEvent<HTMLTextAreaElement>): void {
+    const content = e.target.value
+    const _selectedNote: Note = {...selectedNote!, content} 
+    setSelectedNoteContent(content)
+    _updateNote(_selectedNote)
+  }
+
+  function handleDelete(note: Note): void {
+    const consent: boolean = confirm('Are you sure?')
+    if (consent) {
+      deleteNote(note)
+      setAllNotes()
+      if (selectedNote && note.id === selectedNote!.id) {
+        setSelectedNote(null)
+        setSelectedNoteContent('')
+      }
+    }
+  }
+
+  function setSelectedStyle(noteId: string): any {
+    if (selectedNote && noteId === selectedNote.id) return { backgroundColor: 'green', color: 'white' }
+    return {}
+  }
 
   return (
     <div>
       <h1>Note</h1>
-      <p>Keep your data only for you.</p>
-      <p>No data tranferring through the internet.</p>
-      <p>All data is saved to your machine only.</p>
+      <p>
+        Keep your data only for you -
+        no data tranferring through the internet.
+        All data is saved to your machine only.
+      </p>
       <div style={{ marginBottom: '1rem' }}>
         <Link href="/">Home</Link>
       </div>
       <div style={{ marginBottom: '1rem' }}>
-        <textarea
-          name="note"
-          id="note"
-          cols={30}
-          rows={10}
-          placeholder="Write your note here..."
-          value={note}
-          onChange={e => {
-            const content = e.target.value
-            setNote(content)
-            save(content)
-          }}>
-        </textarea>
+        <h3>Your Notes</h3>
+        <div style={{ marginBottom: '1rem' }}>
+          <input
+            type="text"
+            placeholder='New note name'
+            value={newNoteName}
+            onChange={(e) => setNewNoteName(e.target.value)}
+          />
+          <button onClick={() => handleCreateNote(newNoteName, '')}>Create note</button>
+        </div>
+        <div>
+          {notes.map(note => {
+            return (
+              <div key={note.id} style={setSelectedStyle(note.id)}>
+                <div style={{ display: 'inline-block', marginRight: '0.5rem' }} onClick={() => selectNote(note)}>{note.name}</div>
+                <button onClick={() => handleDelete(note)}>delete</button>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+      <div style={{ marginBottom: '1rem' }}>
+        {selectedNote && (
+          <textarea
+            name="note"
+            id="note"
+            cols={30}
+            rows={10}
+            placeholder="Write your note here..."
+            value={selectedNoteContent}
+            onChange={handleContentChange}>
+          </textarea>
+        )}
       </div>
     </div>
   )
--- source/src/app/note/services/NoteService.ts
+++ source/src/app/note/services/NoteService.ts
@@ -1,9 +1,45 @@
-export function save(content: string): void {
-  localStorage.setItem('note', content)
+import Note from '@/app/note/models/Note'
+import { v4 as uuidv4 } from 'uuid'
+
+const idPrefix = 'note-'
+
+export function saveNote(note: Note): void {
+  const noteWithId: Note = {...note, id: uuidv4() }
+  const _note: string = JSON.stringify(noteWithId)
+  const id: string = noteWithId.id
+  localStorage.setItem(`${idPrefix}${id}`, _note)
 }
 
-export function load(): string {
-  const content: string | null = localStorage.getItem('note')
-  if (content !== null) return content!
-  return '';
+export function updateNote(note: Note): void {
+  const _note: string = JSON.stringify(note)
+  const id: string = note.id
+  localStorage.setItem(`${idPrefix}${id}`, _note)
+}
+
+export function loadNoteById(noteId: string): Note | null {
+  const record: string | null = localStorage.getItem(`${idPrefix}${noteId}`)
+  if (record) {
+    const note: Note = JSON.parse(record!) as Note
+    return note
+  }
+  return null
+}
+
+export function loadAllNote(): Note[] {
+  const keys: string[] = []
+  for (let i = 0; i < localStorage.length; i++) {
+    const key: string | null = localStorage.key(i)
+    if (key) keys.push(key)
+  }
+  const notes: Note[] = keys.filter(key => key.includes('note-')).map(key => {
+    const record: string | null = localStorage.getItem(key)
+    const note: Note = JSON.parse(record!) as Note
+    return note
+  })
+  return notes
+}
+
+export function deleteNote(note: Note): void {
+  const key = `${idPrefix}${note.id}`
+  localStorage.removeItem(key)
 }
